cc1bc7eb219d426cd73383fcb76a17e5552d7ee7
Mauro Moreno <mmauromoreno1@gmail.com>
2023-04-11 17:13:06 -0300

    SpawnManager finished

--- Endless Roguelike/Assets/Scripts/Managers/SpawnManager.cs
+++ Endless Roguelike/Assets/Scripts/Managers/SpawnManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject spawnMarker;
+    
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    [SerializeField] private Transform borderLeft;
+    [SerializeField] private Transform borderRight;
+    [SerializeField] private Transform borderTop;
+    [SerializeField] private Transform borderBottom;
+
+    [SerializeField] private float spawnRate = 3.5f; //TODO randomize
+
+    public void Start()
     {
-        
+        StartCoroutine(SpawnEnemy(spawnRate));
     }
 
-    // Update is called once per frame
-    void Update()
+    //TODO spawn stronger enemies while the game keeps going
+    IEnumerator SpawnEnemy(float interval) 
     {
-        
+        Vector2 spawnPoint = RandomPointWithinLocations();
+
+        //Wait delay seconds when game starts to spawn a enemy
+        yield return new WaitForSeconds(interval);
+          
+        // Show spawn location for one second
+        Object marker = Instantiate(spawnMarker, spawnPoint, Quaternion.identity);
+        yield return new WaitForSeconds(1);
+        Destroy(marker);
+                  
+        // Spawn enemy
+        GameObject newEnemy = (GameObject) Instantiate(enemyPrefabs[0], spawnPoint, Quaternion.identity);
+
+        StartCoroutine(SpawnEnemy(spawnRate));
+    }
+
+    private Vector2 RandomPointWithinLocations()
+    {
+        Vector2 random = new Vector2();
+        random.x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+        random.y = (int)Random.Range(borderTop.position.y, borderBottom.position.y);
+        return random;
     }
 }
