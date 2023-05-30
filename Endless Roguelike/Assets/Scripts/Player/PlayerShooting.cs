853587c0b7ba48fe16c9f70d970f4c3436b55827
Mauro Moreno <mmauromoreno1@gmail.com>
2023-05-30 13:32:53 -0300

    Implemented audio manager

--- Endless Roguelike/Assets/Scripts/Player/PlayerShooting.cs
+++ Endless Roguelike/Assets/Scripts/Player/PlayerShooting.cs
@@ -34,6 +34,7 @@ public class PlayerShooting : MonoBehaviour
         if (fire.ReadValue<float>() > 0f && Time.time > shootInterval)
         {
             Shoot();
+            FindObjectOfType<AudioManager>().PlaySound("PlayerShoot");
             CameraShaker.Invoke(); //Shakes the camera
         }
     }
